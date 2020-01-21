39ebbe7e125015718230b0c43f5722e0a634c1d8
Asterwei <tristan416@gmail.com>
2020-01-21 14:26:15 +0800

    update

--- 6-2-3-1.sql
+++ 6-2-3-1.sql
@@ -1,6 +1,6 @@
 WITH
-  combine_sub as(
-    select 
+  activity_log_with_conversion_flag as (
+    select
       Users,
       OMO_Members,
       Sessions,
@@ -16,31 +16,13 @@ WITH
       hits_count,
       hitnumber,
       pagepath,
-      NUll as action,
-    FROM `ga360-203507.sample_data_GA.action_log`
+      action,
+      sign(sum(case when action ="Subscribe" then 1 else 0 end) over(partition by OMO_Members order by timestamp desc
+      rows between unbounded preceding and current row))
+     as has_conversion
+     FROM `ga360-203507.sample_data_GA.action_log`)
       
-      union all
-   
-   select  
-      NULL as Users,
-      Account__Account_Number as OMO_Members,
-      NULL as Sessions,
-      NULL as Sessions_count,
-      NULL as bounces,
-      Rate_Plan_Charge__Created_Date as timestamp,
-      NULL as Content_Category,
-      NULL as Content_Title,
-      NULL as Event_Category,
-      NULL as Event_Action,
-      NULL as Event_Label,
-      NULL as hit_type,
-      NULL as hits_count,
-      NULL as hitnumber,
-      NULL as pagepath,
-      "Subscribe" as action       
-    FROM `nd-data-poc.zuora.ZuoraSubscribe` )
-
-select *
-from  combine_sub
-where OMO_Members	="5cae0115d0da610295bb58c7"
-order by timestamp
+select 
+  *
+from  activity_log_with_conversion_flag
+order by timestamp desc
